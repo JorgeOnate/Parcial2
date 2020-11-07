566dfe9360c32e9614533695af6cf44e9951a1e0
JorgeOnate <jorge.onate.2017@upb.edu.co>
2020-11-07 09:43:13 -0500

    InitialGameDesign

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction; //vector2 because needs x and y component
+
+    private Rigidbody2D _rigidbody2D;
+    public Vector2 startPosicion;
+
+     
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosicion = transform.position;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        direction = - Vector2.one.normalized;
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = -direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction.y = -direction.y;
+            direction.x = direction.x;
+
+        }
+        
+    }
+
+    public void Reset()
+    {
+        _rigidbody2D.velocity = Vector2.zero;
+        transform.position = startPosicion;
+        direction = Vector2.one.normalized;
+        speed = 4;
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float velocity = 1f;
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] public KeyCode LeftControl;
+    [SerializeField] public KeyCode RightControl;
+    [SerializeField] public KeyCode UpControl;
+    [SerializeField] public KeyCode DownControl;
+    public bool facingRight;
+    public float horizontalValue;
+    
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Flip();
+        if (Input.GetKey(LeftControl))
+        {
+            transform.Translate(x:-velocity,0f,0f);
+        }
+        //Move DOWN
+        else if (Input.GetKey(RightControl))
+        {
+            transform.Translate(velocity,y:0f,0f);
+        }
+        if (Input.GetKey(UpControl))
+        {
+            transform.Translate(x:0f,y:velocity,0f);
+        }
+        //Move DOWN
+        else if (Input.GetKey(DownControl))
+        {
+            transform.Translate(x:0f,y:-velocity,0f);
+        }
+        
+    }
+    void Flip()
+    {
+        if ((horizontalValue > 0 && facingRight) || (horizontalValue < 0 && !facingRight))
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
